5bc285f34cb2ce26f469ac8b3d413dd823caeeb1
donghoony <aru0504@naver.com>
2022-03-17 14:19:45 +0900

    refactor: refactor files

---
+++ src/HTMLCollector.java
@@ -0,0 +1,75 @@
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class HTMLCollector {
+
+    public static void saveXmlAs(Document doc, String path) throws TransformerException, FileNotFoundException {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+
+        DOMSource src = new DOMSource(doc);
+        StreamResult res = new StreamResult(new FileOutputStream(new File(path)));
+        transformer.transform(src, res);
+    }
+
+    public static String getExtension(String fileName){
+        String extension = "";
+        int i = fileName.lastIndexOf('.');
+        if (i >= 0) {
+            extension = fileName.substring(i+1);
+        }
+        return extension;
+    }
+
+    public static void collect() throws TransformerException, IOException, ParserConfigurationException {
+
+        File directory = new File("./resource");
+        File[] contents = directory.listFiles();
+        //        for(File f: contents) System.out.println(f);
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = docBuilder.newDocument();
+
+        Element docs = doc.createElement("docs");
+        doc.appendChild(docs);
+        int doc_id = 0;
+        for(File f: contents){
+            if (getExtension(f.getName()).equals(".html")) continue;
+
+            String docTitle = HTMLParser.getTitle(contents[doc_id]);
+            String docBody = HTMLParser.getBody(contents[doc_id]);
+
+            // <doc id = $doc_id>
+            Element d = doc.createElement("doc");
+            docs.appendChild(d);
+            d.setAttribute("id", Integer.toString(doc_id));
+
+            // title
+            Element title = doc.createElement("title");
+            title.appendChild(doc.createTextNode(docTitle));
+            d.appendChild(title);
+
+            // body
+            Element body = doc.createElement("body");
+            body.appendChild(doc.createTextNode(docBody));
+            d.appendChild(body);
+
+            doc_id++;
+        }
+
+        saveXmlAs(doc, "output/book.xml");
+    }
+}
