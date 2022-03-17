11ffcaba4dec111306dba490768b3aeb9a981a16
donghoony <aru0504@naver.com>
2022-03-17 14:18:03 +0900

    refactor: refactor files

--- src/WordAnalyzer.java
+++ src/WordAnalyzer.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class WordAnalyzer {
+import org.snu.ids.kkma.index.Keyword;
+import org.snu.ids.kkma.index.KeywordExtractor;
+import org.snu.ids.kkma.index.KeywordList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Text;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.TransformerException;
+import java.io.File;
+import java.io.IOException;
+
+public class WordAnalyzer {
+
+    public static void analyze() throws ParserConfigurationException, IOException, SAXException, TransformerException {
+
+        File file = new File("output/book.xml");
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse(file);
+
+        KeywordExtractor ke = new KeywordExtractor();
+
+        NodeList nodeList = document.getElementsByTagName("doc");
+        String[] analyzedList = new String[nodeList.getLength()];
+
+        for(int i = 0; i < nodeList.getLength(); i++){
+            analyzedList[i] = nodeList.item(i).getChildNodes().item(1).getTextContent();
+            KeywordList kl = ke.extractKeyword(analyzedList[i], true);
+            StringBuilder data = new StringBuilder();
+            for(int k = 0; k < kl.size(); k++){
+                Keyword keyword = kl.get(k);
+                data.append(keyword.getString()).append(":").append(keyword.getCnt()).append("#");
+            }
+            document.getElementsByTagName("doc").item(i).getChildNodes().item(1).setTextContent(data.toString());
+        }
+        HTMLCollector.saveXmlAs(document, "output/index.xml");
+    }
 }
