ec36caab0da496d1ac9b9bfa90eefcf093ead911
donghoony <aru0504@naver.com>
2022-03-24 18:45:43 +0900

    refactor: moved recursively mkdir into fileHelper

--- src/FileHelper.java
+++ src/FileHelper.java
@@ -1,3 +1,6 @@
+import java.io.File;
+import java.io.IOException;
+
 public class FileHelper {
     public String getExtension(String fileName){
         String extension = "";
@@ -6,5 +9,12 @@ public class FileHelper {
             extension = fileName.substring(i+1);
         }
         return extension;
+    }
+
+    public File getFile(String path) throws IOException {
+        File f = new File(path);
+        f.getParentFile().mkdirs();
+        f.createNewFile();
+        return f;
     }
 }
