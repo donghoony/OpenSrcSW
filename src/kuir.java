6387e60b07c5b9b3f07bd332651c323179385ead
donghoony <aru0504@naver.com>
2022-04-04 10:25:10 +0900

    fix: print message when there's no matching document

--- src/kuir.java
+++ src/kuir.java
@@ -56,6 +56,7 @@ public class kuir {
 
                 Document document = xmlParser.loadDocument("./index.xml");
                 List<Map.Entry<Integer, Double>> similarity = searcher.CalcSim(path, query);
+                if(similarity.isEmpty()) System.out.println("검색된 문서가 없습니다.");
                 for(Map.Entry<Integer, Double> entry: similarity){
                     System.out.println(document.getElementsByTagName("title").item(entry.getKey()).getTextContent() + " " + entry.getValue());
                 }
